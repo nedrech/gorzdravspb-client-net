f8de3d2774f81de715ac3c6c0d1f6328c9dc16e2
nedrech <denischernyy@ya.ru>
2022-01-21 20:13:55 +0300

    Пример использования в ConsoleApp

--- samples/ConsoleApp/Program.cs
+++ samples/ConsoleApp/Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Nedrech.GorzdravClient;
+using Nedrech.GorzdravClient.Models.Enums;
+
+var api = new GorzdravClient(); // инициализация клиента
+
+var clinics = await api.GetClinicsAsync(); // получение коллекции всех клиник 
+
+var centralClinics = clinics
+    .Where(x => x.DistrictName == DistrictName.Адмиралтейский); // LINQ-фильтрация по району
+
+foreach (var clinic in centralClinics)
+    Console.WriteLine($"[{clinic.Id}]: {clinic.ShortName}"); // вывод в виде "[1]: clinic short name"
