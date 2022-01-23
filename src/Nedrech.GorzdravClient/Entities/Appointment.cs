580a3d75643b4d31d654aa50a4d09eb0d0dd3e4f
nedrech <denischernyy@ya.ru>
2022-01-23 19:28:04 +0300

    Добавлена сущность для номерка

---
+++ src/Nedrech.GorzdravClient/Entities/Appointment.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Nedrech.GorzdravClient.Entities;
+
+/// <summary>
+///     Информация о номерке.
+/// </summary>
+public class Appointment
+{
+    /// <summary>
+    ///     Идентификатор номерка.
+    /// </summary>
+    [JsonPropertyName("id")]
+    public string Id { get; set; } = null!;
+
+    /// <summary>
+    ///     Время посещения.
+    /// </summary>
+    [JsonPropertyName("visitStart")]
+    public DateTimeOffset VisitStart { get; set; }
+
+    /// <summary>
+    ///     Время окончания посещения.
+    /// </summary>
+    [JsonPropertyName("visitEnd")]
+    public DateTimeOffset VisitEnd { get; set; }
+
+    /// <summary>
+    ///     Адрес ЛПУ.
+    /// </summary>
+    [JsonPropertyName("address")]
+    public string Address { get; set; }
+
+    /// <summary>
+    ///     Номер кабинета.
+    /// </summary>
+    [JsonPropertyName("room")]
+    public string? Room { get; set; }
+}
