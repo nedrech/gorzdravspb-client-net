6454bd851255b6cb754a901881c079a48d06b30c
nedrech <denischernyy@ya.ru>
2022-01-23 18:52:33 +0300

    Объединение тестов в один файл

--- tests/Nedrech.GorzdravClient.Tests.Integration/ApiMethodsTests.cs
+++ tests/Nedrech.GorzdravClient.Tests.Integration/ApiMethodsTests.cs
@@ -1,9 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using Nedrech.GorzdravClient.Exceptions;
 using NUnit.Framework;
 
 namespace Nedrech.GorzdravClient.Tests.Integration;
 
-public class SharedMethodsTests
+public class ApiMethodsTests
 {
     private GorzdravClient ApiClient { get; set; } = null!;
 
@@ -11,6 +13,29 @@ public class SharedMethodsTests
     public void SetUp()
     {
         ApiClient = new GorzdravClient();
+    }
+
+    [Test]
+    public void Should_Throws_On_Wrong_Specialty_Id()
+    {
+        var ex = Assert.ThrowsAsync<ApiRequestException>(async () =>
+            await ApiClient.GetSpecialtiesAsync(77777));
+
+        Debug.Assert(ex != null);
+
+        Assert.That(ex.ErrorCode, Is.EqualTo(610));
+    }
+
+    [Test]
+    public async Task It_Should_Give_Out_Specialties()
+    {
+        var specialties = await ApiClient.GetSpecialtiesAsync(1);
+
+        foreach (var specialty in specialties)
+        {
+            Assert.NotNull(specialty.Id);
+            Assert.NotNull(specialty.Name);
+        }
     }
 
     [Test]
