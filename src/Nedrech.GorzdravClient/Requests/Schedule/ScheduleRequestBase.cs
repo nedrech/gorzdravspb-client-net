4bf5aebd49b64bdb78a41e2bb82f150a2432fa1b
nedrech <denischernyy@ya.ru>
2022-01-23 18:29:33 +0300

    Убран неиспользуемый конструктор

--- src/Nedrech.GorzdravClient/Requests/Schedule/ScheduleRequestBase.cs
+++ src/Nedrech.GorzdravClient/Requests/Schedule/ScheduleRequestBase.cs
@@ -2,10 +2,6 @@
 
 public class ScheduleRequestBase : RequestBase
 {
-    protected ScheduleRequestBase(string methodName) : this(methodName, HttpMethod.Get)
-    {
-    }
-
     protected ScheduleRequestBase(string methodName, HttpMethod method)
         : base("schedule/" + methodName, method)
     {
